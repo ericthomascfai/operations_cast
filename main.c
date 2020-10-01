45d14451eb023bac5ab5c8d6139fafb62146e961
eric <eric.thomas@formation-industries-fc.fr>
2020-10-01 14:15:59 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    /************************************Révision********************************************************/
+    double x=0,y=0; //Attention le résultat d'une operation est du même type
+    double s=0;
+    printf("Veuillez saisir la valeur de x\n");
+    scanf("%d",&x);
+    printf("Veuillez saisir la valeur de y\n");
+    scanf("%d",&y);
+    /***************premiere façon**************************/
+    s=x/y;
+    printf("Le résultat de la division de x/y est %0.2lf\n",s); // 0.2 deux chiffres après la virgule
+    /***************deuxième façon***************************/
+    printf("Le résultat de la division de x/y est %0.2lf\n",x/y);
+    /******************************************Exercice n°3**********************************************/
+    int n=0;
+    printf("Veuillez saisir un nombre entier");
+    scanf("%d",&n);
+    printf("opposé:%d\n",n*-1);
+    printf("Moitier exacte: %lf\n",(double)n/2);
+
+
+    return 0;
+
+
+
+}
